374fb82a7ff584a74f388aaf9cc9f2736c0b1469
Anatoly Sokolov <falke2@gmail.com>
2019-11-22 16:30:31 +0300

    Initial commit

---
+++ resources/js/components/QRReader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <qrcode-stream @decode="onDecode"></qrcode-stream>
+    </div>
+</template>
+
+<script>
+    import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
+
+    export default {
+        name: 'QRReader',
+        components: {
+            QrcodeStream
+        },
+        methods: {
+            onDecode(event) {
+                console.log(event);
+            }
+        }
+    }
+</script>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <QRReader></QRReader>123
+    </div>
+</template>
+
+<script>
+
+    import QRReader from '../components/QRReader';
+
+    export default {
+        name: 'App',
+        components: { QRReader },
+    }
+</script>
